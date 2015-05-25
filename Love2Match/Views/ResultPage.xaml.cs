27f3e1e7ab91b128c9dc5a3accecefdb6b0c362c
Didier Gohourou <godidier@msn.com>
2015-05-25 23:31:58 +0000

    Vibration at 100% and ShakeGesture Added

--- Love2Match/Views/ResultPage.xaml.cs
+++ Love2Match/Views/ResultPage.xaml.cs
@@ -7,6 +7,8 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using Windows.Phone.Devices.Notification;
+using ShakeGestures;
 
 using Love2Match.ViewModels;
 //using System.Threading;
@@ -19,88 +21,153 @@ namespace Love2Match.Views
 
         private ResultViewModel vm = new ResultViewModel();
         private DispatcherTimer ScoreAnimationTimer = new DispatcherTimer();
+        private DispatcherTimer ShakeTimer = new DispatcherTimer();
+        private VibrationDevice VibrationDev = VibrationDevice.GetDefault();
 
         private double DisplayedScore;
         private double HeartMaskInitHeight;
+        private byte ShakeBonus = 0;
+        private const byte SHAKE_BONUS_COEF = 3; // A multiplier for shakebonus
+        private const byte SHAKE_BONUS_MAX = 10;
+        private TimeSpan ShakeTime = TimeSpan.FromSeconds(2);
+
 
         public ResultPage()
         {
             InitializeComponent();
+            Loaded += ResultPage_Loaded;
             DataContext = vm;
 
             HeartMaskInitHeight = HeartMaskShape.Height;
-
-            ScoreAnimationTimer.Interval = TimeSpan.FromMilliseconds(5);
-            ScoreAnimationTimer.Tick += AnimateScore;
-            
-            Loaded += (s, e) => 
-            {
-                ScoreAnimationTimer.Start();
-            };
+            ShakeBar.Minimum *= SHAKE_BONUS_COEF;
+            ShakeBar.Maximum *= SHAKE_BONUS_COEF;
         }
+
 
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
 
-            if (NavigationContext.QueryString.ContainsKey("action")) 
+            if (NavigationContext.QueryString.ContainsKey("action"))
             {
-                if (NavigationContext.QueryString["action"] == "calculate") 
+                if (NavigationContext.QueryString["action"] == "calculate")
                 {
-                    ShakePanel.Visibility = Visibility.Visible;
-                    ScorePanel.Visibility = Visibility.Collapsed;
-                    WaitShakeBonus();
-
+                    
                     Lover1Text.Text = MainViewModel.Couple[0].FullName;
                     Lover2Text.Text = MainViewModel.Couple[1].FullName;
-                    WaitShakeBonus();
 
                     vm = new ResultViewModel(MainViewModel.Couple);
                     vm.CalculateLoveScore();
-
-                    ShakePanel.Visibility = Visibility.Collapsed;
-                    ScorePanel.Visibility = Visibility.Visible;
-                    
                 }
             }
         }
 
-        
-        
-        
 
-        private  void WaitShakeBonus() 
+
+        private void ShakeDetected(object sender, ShakeGestureEventArgs e)
         {
-            //ShakeGrid.Visibility = Visibility.Visible;
-            //Thread.Sleep(3000);
-            
-            //ShakeGrid.Visibility = Visibility.Collapsed;
+            this.Dispatcher.BeginInvoke(() =>
+            {
+                if (ShakeBonus < SHAKE_BONUS_MAX * SHAKE_BONUS_COEF) 
+                {
+                    ShakeBonus++;
+                    var bonus = ((double)ShakeBonus / (double)SHAKE_BONUS_COEF);
+                    ShakeBonusTextBox.Text = String.Format("+{0}",  Math.Round(bonus, 1));
+                    ShakeBar.Value = ShakeBonus;
+                    //VibrationDev.Vibrate(TimeSpan.FromSeconds(0.5));
+                }
+            });
         }
-       
 
 
-        void AnimateScore(object sender, EventArgs e)
+        /// <summary>
+        /// Check if ShakeTime span has ended and show the score if so
+        /// Deactivate the ShakeGestureHelper and ShowScore()
+        /// else decrease the ShakeTime TimeSpan
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ShakeTimer_Tick(object sender, EventArgs e)
+        {
+            if (ShakeTime.Seconds <= 0) 
+            {
+                ShakeTimer.Stop();
+                ShakeGesturesHelper.Instance.Active = false;
+                ShowScore();
+            }
+            else 
+            {
+                ShakeTime = TimeSpan.FromSeconds(ShakeTime.Seconds - 1);
+                ShakeTimeTextBox.Text = ShakeTime.Seconds.ToString();
+            }
+        }
+
+
+        private void ShowScore()
+        {
+            ShakePanel.Visibility = Visibility.Collapsed;
+            ScorePanel.Visibility = Visibility.Visible;
+
+            ScoreAnimationTimer.Interval = TimeSpan.FromMilliseconds(5);
+            ScoreAnimationTimer.Tick += AnimateScore;
+            ScoreAnimationTimer.Start();
+        }
+
+
+
+        private void WaitShakeBonus() 
+        {
+            ShakePanel.Visibility = Visibility.Visible;
+            ScorePanel.Visibility = Visibility.Collapsed;
+
+            ShakeGesturesHelper.Instance.ShakeGesture += new EventHandler<ShakeGestureEventArgs>(ShakeDetected);
+            ShakeGesturesHelper.Instance.MinimumRequiredMovesForShake = 2;
+            ShakeGesturesHelper.Instance.Active = true;
+
+            // Shake Controls Initializations
+            ShakeTimeTextBox.Text = String.Format("{0}", ShakeTime.Seconds + 1);
+            ShakeBonusTextBox.Text = "+0";
+            ShakeBar.Value = ShakeBonus;
+
+            ShakeTimer.Interval = TimeSpan.FromSeconds(3);
+            ShakeTimer.Tick += ShakeTimer_Tick;
+            ShakeTimer.Start();
+        }
+
+
+
+        private void AnimateScore(object sender, EventArgs e)
         {
             if (DisplayedScore >= vm.LoveScore) // The score is calculated in OnNavigatedTo
             {
                 ScoreAnimationTimer.Stop();
+                if (vm.LoveScore == 100)
+                {
+                    VibrationDev.Vibrate(TimeSpan.FromSeconds(1));
+                }
             }
-            else 
+            else
             {
                 HeartMaskShape.Height -= (HeartMaskInitHeight / 100); // remove one unit of the percentage of initial height
                 DisplayedScore++;
                 ScoreTextBlock.Text = DisplayedScore + "%";
             }
-#if(DEBUG) 
-            System.Diagnostics.Debug.WriteLine("LoveScore='"+vm.LoveScore+"', "
-                +"Displayed Score='"+DisplayedScore+"', "
-                +"MaskHeight='"+HeartMaskShape.Height+"'");
+#if(DEBUG)
+            System.Diagnostics.Debug.WriteLine("LoveScore='" + vm.LoveScore + "', "
+                + "Displayed Score='" + DisplayedScore + "', "
+                + "MaskHeight='" + HeartMaskShape.Height + "'");
 
 #endif
         }
 
-       
+
+        void ResultPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            WaitShakeBonus();
+        }
+
+
 
         private void AppBarSave_Click(object sender, EventArgs e)
         {
